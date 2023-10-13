8adf84d5c28b758216dff0c376807adfc3072956
chirrylun <134626109+chirrylun@users.noreply.github.com>
2023-10-13 09:08:27 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { NavBar } from './components/navBar';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import React from 'react';
 import './App.css';
 
 function App() {
+  
+  
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact")
+      .then((res) => {
+        setCatFact(res.data.fact);
+      })
+      .catch((error) => {
+        console.error("Error fetching cat fact:", error);
+      });
+  };
+
+  useEffect(() =>{
+    fetchCatFact();
+    console.log("Mounted")
+
+    
+  
+    
+  },[]);
+  const [catFact, setCatFact] = useState("");
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={fetchCatFact}>Generate Fact</button>
+    <p className='factText'>{catFact}</p>
+    <p>{text}</p>
+      
     </div>
   );
 }
---
+++ src/components/navBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return(
+        <div>
+            <ul>
+            <img src='/images/logo.png' className='logo' alt='Logo' />
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            </ul>
+        </div>
+    );
+
+};
